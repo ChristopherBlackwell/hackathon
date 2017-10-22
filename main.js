6288bab516be7f045ca1f951f84d15862a70f849
Chris Blackwell <Chris Blackwell>
2017-10-21 22:21:30 -0400

    visibility of table and picture is dependent on image upload. Also introduced jquery"

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-var after_upload = 0;
+$(document).ready(function(){
+  var after_upload = 0;
 
-if (after_upload == 0)
-{
-  document.getElementById('pic_display').style.display = 'none';
-}
+  if (after_upload == false)
+  {
+    $('#pic_display').hide();
+    $('#table_display').hide();
+  }
+
+  $("input[type=file]").click(function(){
+      $(this).val("");
+  });
+
+  $("input[type=file]").change(function(){
+      after_upload = $(this).val();
+
+      if (after_upload == false)
+      {
+        $('#pic_display').hide();
+        $('#table_display').hide();
+      }
+      else
+      {
+        $('#pic_display').show();
+        $('#table_display').show();
+      }
+
+  });
+});
