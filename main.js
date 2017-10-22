212ad5884d5c12da5ddc0dc276bd0997462c1c00
Pat Sweetman <sweetmantech@gmail.com>
2017-10-22 09:39:31 -0400

    added firebase Realtime Database

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
-$(document).ready(function()
-{
-  /*var after_upload = 0;
-
-  if (after_upload == false)
-  {
-    $('#pic_display').hide();
-    $('#table_display').hide();
-  }
-
-  $("input[type=file]").click(function()
-  {
-    $(this).val("");
+$(document).ready(function(){
+  // Get a reference to the database service
+  var database = firebase.database().ref('images/mostRecent/text');
+  database.on('value', function(snapshot) {
+    var brand = snapshot.child("brand").val();
+    var catalog = snapshot.child("catalogue").val();
+    var order = snapshot.child("orderNum").val();
+    var raw = snapshot.child("raw").val();
+    $("#brand_var").text(brand);
+    $("#catalog_var").text(catalog);
+    $("#GO_var").text(order);
+    $("#raw_var").text(raw)
   });
-
-  $("input[type=file]").change(function(){
-    after_upload = $(this).val();
-
-    if (after_upload == false)
-    {
-      $('#pic_display').hide();
-      $('#table_display').hide();
-    }
-    else
-    {
-      $('#pic_display').show();
-      $('#table_display').show();
-    }
-
-  });*/
-
-})
+});
